e2ee608e1eeb21a5e4ad6ae8e681a9cdbbbc5955
Kaixin <kaixinji4798@gmail.com>
2019-11-12 21:05:14 +1030

    Add files via upload

--- DTran/DTran.cpp
+++ DTran/DTran.cpp
@@ -10,132 +10,108 @@ void DTran::begin(Stream &port) {
 void DTran::setBaudrate(uint16_t baud) {
 	_mySerial->println("AT+IPREX=" + baud);
 	updateSerial();
-	setUp();
 }
 
 void DTran::setUp(){
 	Serial.println("Initializing...");
-	delay(1000);
-
-	//_mySerial->println("AT"); //Once the handshake test is successful, it will back to OK
-	//updateSerial();
-	//_mySerial->println("AT+CSQ"); //Signal quality test, value range is 0-31 , 31 is the best
-	//updateSerial();
-	//_mySerial->println("AT+CCID"); //Read SIM information to confirm whether the SIM is plugged
-	//updateSerial();
-	//_mySerial->println("AT+CREG?"); //Check whether it has registered in the network
-	//updateSerial();
-	_mySerial->println("AT+CGSOCKCONT=1,\"IP\",\"telstra.wap\"");
+	delay(500);
+	_mySerial->println("AT"); //Once the handshake test is successful, it will back to OK
 	updateSerial();
 
-	_mySerial->println("AT+CMGF=1");
+	if (!checkReply("OK")) {
+		Serial.println("Power off the 3G sheild and reset the Controller, Please.");
+		status = false;
+		return;
+	}
+
+	_mySerial->println("AT+CSQ"); //Signal quality test, value range is 0-31 , 31 is the best
+	updateSerial();
+	_mySerial->println("AT+CCID"); //Read SIM information to confirm whether the SIM is plugged
+	updateSerial();
+	_mySerial->println("AT+CREG?"); //Check whether it has registered in the network
+	updateSerial();
+	_mySerial->println("AT+CMGF=1"); //Configure SMS mode
+	updateSerial();
+	_mySerial->println("AT+CGSOCKCONT=1,\"IP\",\"telstra.wap\""); //Set up the barrier profile
 	updateSerial();
 	_mySerial->println("AT+CSOCKSETPN=1");
 	updateSerial();
-	//_mySerial->println("AT+SAPBR=3,1,\"APN\",\"telstra.wap\"");
-	//updateSerial();
-	//_mySerial->println("AT+SAPBR=1,1");
-	//updateSerial();
  }
 
-
-void DTran::sendMessage(){
+void DTran::sendMessage(String phone_number, String message){
+	if (!status) {return;}
+	Serial.println();
 	Serial.println("Sending the message");
-	Serial.println("Enter a phone number to send: (including in \" \" )");
-	String phone_number = "";
-	String message = "";
-	while (phone_number == "") {
-		if (Serial.available()) {
-			phone_number = Serial.readString();
-		}
-	}
-	
-	if (phone_number == "") {
-		Serial.println("Phone number is empty");
-	}
-	else {
-		_mySerial->println("AT+CMGS=" + phone_number);
-		delay(100);
-		Serial.println("Enter a message to send: ");
-		while (message == "") {
-			if (Serial.available()) {
-				message = Serial.readString();
-			}
-		}
-		_mySerial->println(message);
-		_mySerial->write(0x1a);
-	}
+	_mySerial->println("AT+CMGS=\"" + phone_number + "\"");
+	updateSerial();
+	_mySerial->println(message);
+	_mySerial->write(0x1a);
+	updateSerial();
 
+	delay(1000);
+	bool result = checkReplyTimeout(2000, "OK");
+	if (!result) {
+		Serial.println(MESSAGE_SMS_FAILED);
+		return;
+	} 
 }
 
-//bool DTran::sendHTTP(String request, String host, String type, String len, String params) {
-//	_mySerial->println("AT+CHTTPSSTART");
-//	updateSerial();
-//	delay(1000);
-//
-//	_mySerial->println("AT+CHTTPSOPSE=\"myarduinosite.tk\", 80, 1");
-//	updateSerial();
-//	
-//	_mySerial->println("AT+CHTTPSSEND="+ String(request.length()+host.length()+1) );
-//	updateSerial();
-//
-//	_mySerial->println(request);
-//	_mySerial->println(host);
-//	_mySerial->write(0x1a);
-//	//_mySerial->println(len + String(0));
-//	updateSerial();
-//
-//	updateSerial();
-//
-//	_mySerial->println("AT+CHTTPSSEND");
-//	updateSerial();
-//	delay(300);
-//	//_mySerial->println("AT+CHTTPSRECV?");
-//	//updateSerial();
-//
-//	//closeHTTP();
-//	return true;
-//	//return strstr(replyBuffer, "ok") != 0 ;
-//}
+/* This method can not be bool type, otehrwise the HTTP function does not sent
+or the AT command does not work*/
+void DTran::sendHTTP(String url, String request) {
+	if (!status) {return;}
 
-bool DTran::sendHTTP(String request, String host, String type, String len, String params) {
-	_mySerial->println("AT+CHTTPACT=\"myarduinosite.tk\",80");
-	delay(1000);
 	updateSerial();
-	delay(100);
-	_mySerial->println(request);
+	delay(500);
+
+	//open HTTP session
+	_mySerial->println("AT+CHTTPACT=\""+ url + "\",80");
+
+	//* important: update serial twice because there are two reponse 
+	updateSerial();
+	delay(300);
+	updateSerial();
+
+	bool result = checkReplyTimeout(3000, "REQUEST");
+
+	if (!result) {
+		Serial.println(MESSAGE_FAILED);
+		digitalWrite(LEDPIN, HIGH);
+		sendMessage(PHONE_NUMBER, MESSAGE_FAILED);
+		return;
+	}
+
+	_mySerial->println("POST " + request + writeBuffer + " HTTP/1.1");
 	delay(1);
-	_mySerial->println(host);
+	_mySerial->println("Host: " + url);
 	delay(1);
-	_mySerial->println(type);
+	_mySerial->println("Content-Type: application/x-www-form-urlencoded; charset=UTF-8");
 	delay(1);
-	_mySerial->println(len + String(255));
+	_mySerial->println("Content-Length:" + String(255));
 	delay(1);
 	_mySerial->println();
-	_mySerial->println(params);
-	delay(1);
 	_mySerial->write(0x1a);
-	delay(1000);
-	updateSerial();
 
-	delay(1000);
 	updateSerial();
+	result = checkReplyTimeout(10000, "ok");
 
-	return true;
-	//return strstr(replyBuffer, "ok") != 0 ;
+	updateSerial();
+	if (!result) {
+		Serial.println(MESSAGE_UPLOAD_FAILED);
+		digitalWrite(LEDPIN, HIGH);
+		sendMessage(PHONE_NUMBER, MESSAGE_FAILED);
+		return;
+	}
+	else {
+		Serial.println();
+		sendMessage(PHONE_NUMBER, MESSAGE_SUCCESS);
+	}
 }
-
-void DTran::closeHTTP() {
-	_mySerial->println("AT+CHTTPSCLSE");
-	updateSerial();
-	_mySerial->println("AT+CHTTPSSTOP");
-	updateSerial();
-}
-
 
 void DTran::updateSerial()
 {
 	delay(500);
+
 	for (int i = 0; i < sizeof(replyBuffer); i++) {
 		replyBuffer[i] = 0;
 	}
@@ -157,6 +133,71 @@ void DTran::updateSerial()
 		char a = _mySerial->read();
 		Serial.write(a);
 		replyBuffer[pos] = a;
+		pos += 1;
+	}
+}
+
+bool DTran::reset() {
+	Serial.println("Waiting for 3 seconds to reset...");
+	delay(3000);
+	Serial.println("Resetting...");
+	_mySerial->println("AT+CRESET");
+	updateSerial();
+	while (!checkReply("PB DONE")) {
+		updateSerial();
+	}
+
+	Serial.println("AT+CRESET?");
+	updateSerial();
+	return checkReply("OK");
+}
+
+bool DTran::checkReplyTimeout(int timeout, const char* expect) {
+	delay(1000);
+	Serial.println();
+	bool result = false;
+	while (timeout > 0) {
+		Serial.println("Waiting ..... " + String(timeout / 500));
+		updateSerial();
+		if (checkReply(expect)) {
+			result = true;
+			break;
+		}
+		timeout -= 500;
+	}
+	return result;
+}
+
+bool DTran::checkReply(const char* expect) {
+	return (strstr(replyBuffer, expect) != 0);
+}
+
+void DTran::decodeParameter(String params[]) {
+	if (!status) { return; }
+	for (int i = 0; i < sizeof(writeBuffer); i++) {
+		writeBuffer[i] = 0;
+	}
+	writeBuffer[0] = '?';
+	int pos = 1;
+	for (int i=0; i <= sizeof(params); i++) {
+		for (char a : params[i]) {
+			if (a == ' ') {
+				writeBuffer[pos] = '+';
+			}
+			else if (a == '+') {
+				writeBuffer[pos] = '%';
+				pos += 1;
+				writeBuffer[pos] = '2';
+				pos += 1;
+				writeBuffer[pos] = 'B';
+			}
+			else {
+				writeBuffer[pos] = a;
+			}
+			pos += 1;
+		}
+		if (i == sizeof(params)) { break; }
+		writeBuffer[pos] = '&';
 		pos += 1;
 	}
 }
--- DTran/DTran.h
+++ DTran/DTran.h
@@ -9,6 +9,16 @@
 
 #include <SoftwareSerial.h>
 
+#define LEDPIN 13
+
+//Customise the phone number and message
+#define PHONE_NUMBER "+61422540224"
+#define MESSAGE_SUCCESS "Data Uploaded"
+#define MESSAGE_FAILED "HTTP Failed"
+#define MESSAGE_UPLOAD_FAILED "Upload Failed"
+#define MESSAGE_SMS_FAILED "SMS Failed"
+
+
 class DTran
 {
   public:
@@ -17,14 +27,19 @@ class DTran
 	void begin(Stream &port);
 	void setBaudrate(uint16_t baud);
 	void setUp();
-	void sendMessage();
-	bool sendHTTP(String request, String host, String type, String len, String params);
+	void sendMessage(String phone_number, String message);
+	void sendHTTP(String url, String request);
 	void updateSerial();
-	void closeHTTP();
+	bool reset();
+	bool checkReplyTimeout(int timeout, const char* expect);
+	bool checkReply(const char* expect);
+	void decodeParameter(String params[]);
 
 private:
 	Stream *_mySerial;
 	char replyBuffer[255];
+	char writeBuffer[255];
+	bool status = true;
     
 };
 
